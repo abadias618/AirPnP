e381b765809b5ca1a6f4fe2360e66490befa0f01
abdias <abadias618@gmail.com>
2019-09-25 13:49:57 -0600

    Add project files.

---
+++ AirPnP.MobileAppService/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirPnP.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ AirPnP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AirPnP.Models;
+
+namespace AirPnP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
