5eaaf9f34301131b249f851fcb5027e80e0a23e3
Ivanfeliper <47649807+Ivanfeliper@users.noreply.github.com>
2019-11-25 10:03:45 -0700

    IvanRegino 11/25/2019
    This is what I have so far for the project

---
+++ AirPnP/AirPnP/Profile.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navegacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+
+        }
+        void Description (object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new DescriptionDetail());
+
+        }
+    }
+}
